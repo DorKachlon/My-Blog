b7ac3409bc1d822aaa8a0b9fa4d9e5ddcbbd7ebe
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-10-14 20:05:44 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Suspense, lazy } from "react";
+import "./App.css";
+import Article from "./Article";
+import ErrorBoundary from "./ErrorBoundary";
+const Comments = lazy(() => import("./Comments"));
+
+const Loading = () => <div style={{ color: "white" }}>Loading . . . </div>;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <h1 className="webName">BiLA BLOG</h1>
+            <div className="container">
+                <div className="page">
+                    <Article />
+                </div>
+                <Suspense fallback={<Loading />}>
+                    <ErrorBoundary>
+                        <Comments />
+                    </ErrorBoundary>
+                </Suspense>
+            </div>
+        </>
+    );
 }
 
 export default App;
