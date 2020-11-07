314f7e0424594505998b49b409a3f09fc9a44d02
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-11-07 19:02:21 +0200

    1

---
+++ src/Background.js
@@ -0,0 +1,5 @@
+import React from "react";
+import background from "./background.png";
+export default function Background() {
+    return <img style={{height:"100vh", width:"100vw"}} src={background} />;
+}
