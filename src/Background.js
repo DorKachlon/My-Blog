8fa9ab816af116ffbaeb0bd6dc2cd28d28a6af62
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-11-09 20:29:35 +0200

    remove background

--- src/Background.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import background from "./background.png";
-export default function Background() {
-    return <img style={{height:"100vh", width:"100vw"}} src={background} />;
-}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Background from "./Background";
+
 ReactDOM.render(
     <React.StrictMode>
-        {/* <App /> */}
-        <Background />
+        <App />
     </React.StrictMode>,
     document.getElementById("root")
 );
